9f0c335407b9b5361150ab2dee4bf2daf9fe904f
subhayan <subhayan.m5@gmail.com>
2020-08-28 20:50:12 +0530

    second commit

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -11,8 +11,8 @@ class Location{
       latitude = position.latitude;
       longitude=position.longitude;
     }
-    catch(e){
-      print(e);
+    catch(exception){
+      print(exception);
     }
   }
 }
