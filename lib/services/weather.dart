f9cfd7ddb1760acb33db155d7b0a6dccc13dfdc3
subhayan <subhayan.m5@gmail.com>
2020-09-05 16:48:17 +0530

    third commit
    major changes in ui and functionality

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,59 +1,42 @@
+//class to implement asynchronous functions that take locations from different sources
+//(geolocator and current weather api call) and returns the weather data.
+
 import 'package:weatherapp/services/location.dart';
 import 'package:weatherapp/services/networking.dart';
 
 const apiKey = 'YOUR-KEY-HERE';
-const openWeatherMapURL = 'http://api.openweathermap.org/data/2.5/weather';
+const openWeatherMapURLOneCall =
+    'https://api.openweathermap.org/data/2.5/onecall';
+const openWeatherMapURLCurrent =
+    'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
-
-  Future<dynamic> getCityWeather(String cityName) async {
-    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
-    NetworkHelper networkHelper = NetworkHelper(url);
-    var weatherData = await networkHelper.getData();
-    return weatherData;
-  }
-
-  Future<dynamic> getLocationWeather() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-
-    NetworkHelper networkHelper = NetworkHelper(
-        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-    var weatherData = await networkHelper.getData();
-
-    return weatherData;
-  }
-
-  String getWeatherIcon(int condition) {
-    if (condition < 300) {
-      return '🌩';
-    } else if (condition < 400) {
-      return '🌧';
-    } else if (condition < 600) {
-      return '☔️';
-    } else if (condition < 700) {
-      return '☃️';
-    } else if (condition < 800) {
-      return '🌫';
-    } else if (condition == 800) {
-      return '☀️';
-    } else if (condition <= 804) {
-      return '☁️';
-    } else {
-      return '🤷‍';
+  var weatherData;
+  double lat;
+  double lon;
+  Future<void> getCurrentLocationWeather() async {
+    try {
+      Location location = Location();
+      await location.getCurrentLocation();
+      lat = location.latitude;
+      lon = location.longitude;
+      NetworkHelper networkHelper = NetworkHelper(
+          '$openWeatherMapURLOneCall?lat=$lat&lon=$lon&%20&appid=$apiKey&units=metric');
+      weatherData = await networkHelper.getData();
+    } catch (e) {
+      print('getCurrentLocationWeather: $e');
     }
   }
 
-  String getMessage(int temp) {
-    if (temp > 25) {
-      return 'It\'s 🍦 time';
-    } else if (temp > 20) {
-      return 'Time for shorts and 👕';
-    } else if (temp < 10) {
-      return 'You\'ll need 🧣 and 🧤';
-    } else {
-      return 'Bring a 🧥 just in case';
+  Future<dynamic> getCityWeather(double latitude, double longitude) async {
+    try {
+      NetworkHelper networkHelper = NetworkHelper(
+          '$openWeatherMapURLOneCall?lat=$latitude&lon=$longitude&%20&appid=$apiKey&units=metric');
+      weatherData = await networkHelper.getData();
+
+      return weatherData;
+    } catch (e) {
+      print('getCityWeather: $e');
     }
   }
 }
